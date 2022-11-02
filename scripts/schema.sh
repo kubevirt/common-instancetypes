803cd32ed868d9edd706635f6b6fad67d77ee837
Lee Yarwood <lyarwood@redhat.com>
2022-11-02 17:05:42 +0000

    scripts: Add dep checks to each script
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+if !command -v openapi2jsonschema &> /dev/null; then
+    echo "openapi2jsonschema is not installed, see https://github.com/mikefarah/yq#install for more details."
+    exit 1
+fi
+
 KUBEVIRT_SWAGGER_URL=https://raw.githubusercontent.com/kubevirt/kubevirt/${KUBEVIRT_VERSION}/api/openapi-spec/swagger.json
 SCHEMA_DIR=schemas/${KUBEVIRT_VERSION}
-
 mkdir -p ${SCHEMA_DIR}
 
 openapi2jsonschema --stand-alone --expanded --strict -o ${SCHEMA_DIR} ${KUBEVIRT_SWAGGER_URL}
