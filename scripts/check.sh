403eef5b67f4b40d41f2a7de30e37ffa0af4fd25
Lee Yarwood <lyarwood@redhat.com>
2022-11-04 11:51:26 +0000

    check: Add pwd as safe git directory
    This is required when running the script within a container image or
    prow job otherwise the following error is shown:
    
    ```
    $ COMMON_INSTANCETYPES_CRI=docker make check
    ./scripts/cri.sh  "./scripts/check.sh"
    warning: Not a git repository. Use --no-index to compare two paths outside a working tree
    ```
    
    A previous attempt to fix this in the Dockerfile is removed as the
    assumed path of the repo changes between local docker runs and prow
    jobs.
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# We need to add the current directory as a safe git directory before running any commands
+git config --global --add safe.directory "$(pwd)"
+
 git diff --quiet common*.yaml README.md
 uncommited=$?
 if [[ $uncommited -eq 1 ]]; then
