93da72e00c0d6e0523020f79578bfa88c6dcf7ee
Lee Yarwood <lyarwood@redhat.com>
2022-11-04 13:55:42 +0000

    cri: Use private unshared label for bind mount
    Required by podman, otherwise we cannot run scripts within the mount.
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- scripts/cri.sh
+++ scripts/cri.sh
@@ -5,4 +5,4 @@ if [ "${COMMON_INSTANCETYPES_CRI}" == "" ]; then
     exit $?
 fi
 
-${COMMON_INSTANCETYPES_CRI} run -v "$(pwd):/common-instancetypes" -e KUBEVIRT_VERSION=${KUBEVIRT_VERSION} --rm ${COMMON_INSTANCETYPES_IMAGE} /usr/bin/bash -c "cd /common-instancetypes &&  $@"
+${COMMON_INSTANCETYPES_CRI} run -v "$(pwd):/common-instancetypes:rw,Z" -e KUBEVIRT_VERSION=${KUBEVIRT_VERSION} --rm ${COMMON_INSTANCETYPES_IMAGE} /usr/bin/bash -c "cd /common-instancetypes &&  $@"
