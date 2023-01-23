b1640f79bf534415ded12d2d572e5b866e3ca000
Lee Yarwood <lyarwood@redhat.com>
2023-01-23 16:11:20 +0000

    check: Check if safe.directory is already configured before adding an entry (#23)
    `git config --global --add` will always add a new line to
    `safe.directory` when called so check if it's actually required before
    calling.
    
    The check for uncommitted changed to generated files is also in-lined.
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # We need to add the current directory as a safe git directory before running any commands
-git config --global --add safe.directory "$(pwd)"
+if ! git config --global --get-all safe.directory | grep "$(pwd)" > /dev/null; then
+    git config --global --add safe.directory "$(pwd)"
+fi
 
-git diff --quiet common*.yaml README.md
-uncommited=$?
-if [[ $uncommited -eq 1 ]]; then
+if ! git diff --quiet common*.yaml README.md; then
     echo "Uncommited changes to generated files present, please commit these and try again."
     git --no-pager diff  common*.yaml README.md
     exit 1
