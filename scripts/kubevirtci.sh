77120b975723a90c4ab9d301346977536f0c3396
Felix Matouschek <fmatouschek@redhat.com>
2023-11-21 14:01:33 +0100

    fix: Provide necessary resources and featureGates for functest
    Raise the memory of the kubevirtci cluster to 16G to provide the
    necessary resources for functional tests. Add the NUMA feature gate to
    'make cluster-up' (was already present in 'make kubevirt-up').
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- scripts/kubevirtci.sh
+++ scripts/kubevirtci.sh
@@ -16,6 +16,7 @@
 
 set -ex
 
+export KUBEVIRT_MEMORY_SIZE="${KUBEVIRT_MEMORY_SIZE:-16G}"
 export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-$(curl -sfL https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirtci/latest)}
 export KUBEVIRT_DEPLOY_CDI="true"
 
@@ -57,8 +58,8 @@ function kubevirtci::up() {
   echo "waiting for kubevirt to become ready, this can take a few minutes..."
   ${_kubectl} -n kubevirt wait kv kubevirt --for condition=Available --timeout=15m
 
-  echo "enabling the GPU feature gate to validate the GN instance types"
-  ${_kubectl} patch kv/kubevirt -n kubevirt --type merge -p '{"spec":{"configuration":{"developerConfiguration":{"featureGates": ["GPU"]}}}}'
+  echo "enabling the GPU and NUMA feature gates to validate instance types"
+  ${_kubectl} patch kv/kubevirt -n kubevirt --type merge -p '{"spec":{"configuration":{"developerConfiguration":{"featureGates": ["GPU", "NUMA"]}}}}'
 }
 
 function kubevirtci::down() {
