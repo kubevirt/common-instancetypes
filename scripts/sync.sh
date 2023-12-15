ad657f136055de1b654925257429b4707862b0d3
Felix Matouschek <fmatouschek@redhat.com>
2023-12-15 16:26:21 +0100

    fix: Ensure old resources are deleted when running sync targets
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -20,6 +20,7 @@ if [ -z "${KUBECTL}" ]; then
     echo "${BASH_SOURCE[0]} expects the following env variables to be provided: KUBECTL."
     exit 1
 fi
-${KUBECTL} delete all -l instancetype.kubevirt.io/vendor=kubevirt.io
+${KUBECTL} delete VirtualMachineClusterInstancetypes -l instancetype.kubevirt.io/vendor=kubevirt.io
+${KUBECTL} delete VirtualMachineClusterPreferences -l instancetype.kubevirt.io/vendor=kubevirt.io
 ${KUBECTL} kustomize VirtualMachineClusterInstancetypes | ${KUBECTL} apply -f -
 ${KUBECTL} kustomize VirtualMachineClusterPreferences | ${KUBECTL} apply -f -
