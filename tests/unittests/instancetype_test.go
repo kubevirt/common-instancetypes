bccbb8b2343cd5374bd612a1ca98cd1c07c3a770
Lee Yarwood <lyarwood@redhat.com>
2024-07-12 10:01:33 +0100

    tests: Stop copying loaded resources for each test
    None of our tests mutate the loaded resources so this isn't required.
    
    Previously none of the loaded resources were copied into the test
    resource slices anyway as the associated slices were left as nil and
    thus the call to copy didn't actually copy anything.
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- tests/unittests/instancetype_test.go
+++ tests/unittests/instancetype_test.go
@@ -20,11 +20,6 @@ const (
 
 var _ = Describe("Common instance types unit tests", func() {
 
-	var (
-		testsClusterPreferences  []instancetypev1beta1.VirtualMachineClusterPreference
-		testsClusterInstanceType []instancetypev1beta1.VirtualMachineClusterInstancetype
-	)
-
 	skipLabels := map[string]bool{
 		"instancetype.kubevirt.io/os-type":                      true,
 		"instancetype.kubevirt.io/deprecated":                   true,
@@ -49,21 +44,15 @@ var _ = Describe("Common instance types unit tests", func() {
 		"instancetype.kubevirt.io/vendor": preferenceCheckVendor,
 	}
 
-	BeforeEach(func() {
-		// Restore resources to make sure we have unchanged data
-		copy(testsClusterPreferences, loadedVirtualMachineClusterPreferences)
-		copy(testsClusterInstanceType, loadedVirtualMachineClusterInstanceTypes)
-	})
-
 	Context("VirtualMachineClusterPreference", func() {
 		It("check version", func() {
-			for _, preference := range testsClusterPreferences {
+			for _, preference := range loadedVirtualMachineClusterPreferences {
 				Expect(preference.APIVersion).To(Equal(expectedVersion))
 			}
 		})
 
 		It("check if labels match resources", func() {
-			for _, preference := range testsClusterPreferences {
+			for _, preference := range loadedVirtualMachineClusterPreferences {
 				for key, value := range preference.Labels {
 					if skipLabels[key] {
 						continue
@@ -78,13 +67,13 @@ var _ = Describe("Common instance types unit tests", func() {
 
 	Context("VirtualMachineClusterInstanceType", func() {
 		It("check version", func() {
-			for _, instanceType := range testsClusterInstanceType {
+			for _, instanceType := range loadedVirtualMachineClusterInstanceTypes {
 				Expect(instanceType.APIVersion).To(Equal(expectedVersion))
 			}
 		})
 
 		It("check if labels match resources", func() {
-			for _, instanceType := range testsClusterInstanceType {
+			for _, instanceType := range loadedVirtualMachineClusterInstanceTypes {
 				for key, value := range instanceType.Labels {
 					if skipLabels[key] {
 						continue
