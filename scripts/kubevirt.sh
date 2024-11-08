84ee25d6eaf8065521428b7efffb778cb90fe958
Lee Yarwood <lyarwood@redhat.com>
2024-11-08 17:25:22 +0000

    kubevirt, kubevirtci: Correctly disable commonInstancetypesDeployment
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- scripts/kubevirt.sh
+++ scripts/kubevirt.sh
@@ -39,7 +39,7 @@ function kubevirt::up() {
   make cluster-up -C "${_base_dir}/_kubevirt" && make cluster-sync -C "${_base_dir}/_kubevirt"
 
   echo "enabling feature gates to validate instance types and preferences"
-  ${_kubectl} patch kv/kubevirt -n kubevirt --type merge -p '{"spec":{"configuration":{"commonInstancetypesDeployment": {"enable": false},"developerConfiguration":{"featureGates": ["GPU", "NUMA", "VMPersistentState"]},"vmStateStorageClass":"nfs-csi"}}}'
+  ${_kubectl} patch kv/kubevirt -n kubevirt --type merge -p '{"spec":{"configuration":{"commonInstancetypesDeployment": {"enabled": false},"developerConfiguration":{"featureGates": ["GPU", "NUMA", "VMPersistentState"]},"vmStateStorageClass":"nfs-csi"}}}'
 
   echo "waiting for kubevirt to become ready, this can take a few minutes..."
   ${_kubectl} -n kubevirt wait kv kubevirt --for condition=Available --timeout=15m
